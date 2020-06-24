d54714cfa879792508e1afa0d0ed100562b0966c
antdp425 <antdp425@aol.com>
2020-06-24 14:39:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+let dodger = document.getElementById("dodger")
+let leftSideEdge = dodger.style.left.replace("px","")
+let left = parseInt(leftSideEdge,10)
+
+addEventListener("keydown", function(e){
+   if (e.key === "ArrowLeft"){
+      moveDodgerLeft();
+   } else if (e.key === "ArrowRight"){
+      moveDodgerRight();
+   }
+})
+
+function moveDodgerLeft() {
+   if (left > 0){
+      dodger.style.left = `${left - 1}px`  
+   }
+}
+
+function moveDodgerRight() {
+   if (left < 360){
+      dodger.style.left = `${left + 1}px`  
+   }
+}
